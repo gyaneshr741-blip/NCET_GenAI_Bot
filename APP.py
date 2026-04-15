f57eca5083d120c5a328f386d422b36a995a7b06
gyaneshr741-blip <gyaneshr741@gmail.com>
2026-04-15 10:43:20 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from greg import Greg
 
-st.set_page_config("PrapgenAI Content Generator", layout="wide")
+st.set_page_config("GyaneshAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
 st.image("141710a660712f1ed66c049b5e6ed224.jpg")
