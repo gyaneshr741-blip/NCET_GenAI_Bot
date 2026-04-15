b7ab239ee57f3a5bc4001404319ef98d6fe4d1dc
gyaneshr741-blip <gyaneshr741@gmail.com>
2026-04-15 10:42:14 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -3,4 +3,4 @@ from greg import Greg
 
 st.set_page_config("PrapgenAI Content Generator", layout="wide")
 st.title("PrapgenAI - Content Generator")
-st.image("C:\Users\Gyanesh\Downloads\WhatsApp Image 2026-02-24 at 10.42.05 AM.jpeg")
+st.image("141710a660712f1ed66c049b5e6ed224.jpg")
