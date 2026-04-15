0f4fe29872a7544914fda718b74caf4529023f92
gyaneshr741-blip <gyaneshr741@gmail.com>
2026-04-15 10:35:08 +0530

    Create APP.py

---
+++ APP.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from greg import Greg
+
+st.set_page_config("PrapgenAI Content Generator", layout="wide")
+st.title("PrapgenAI - Content Generator")
+st.image("C:\Users\Gyanesh\Downloads\WhatsApp Image 2026-02-24 at 10.42.05 AM.jpeg")
