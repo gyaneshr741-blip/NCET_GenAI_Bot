d634d76cd924085292a0e8811a6e384fb4d4d437
gyaneshr741-blip <gyaneshr741@gmail.com>
2026-04-15 10:50:22 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from greg import Greg
+from groq import Groq
 
 st.set_page_config("GyaneshAI Content Generator", layout="wide")
-st.title("PrapgenAI - Content Generator")
+st.title("GyaneshAI - Content Generator")
 st.image("141710a660712f1ed66c049b5e6ed224.jpg")
